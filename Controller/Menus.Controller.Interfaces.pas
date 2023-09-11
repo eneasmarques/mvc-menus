72b3bc3cafd42c9d4ab33af913faddd50a9e7490
eneasmarques <eneasmarques@outlook.com.br>
2023-09-11 06:45:55 -0300

    feat: adiona Factory de criação de Objetos ListBox e ListBoxItens

--- Controller/Menus.Controller.Interfaces.pas
+++ Controller/Menus.Controller.Interfaces.pas
@@ -1,6 +1,14 @@
 unit Menus.Controller.Interfaces;
 
 interface
+  type
+  iControllerListBoxItensFactory = interface
+    ['{EE69FC84-2516-4A0C-B681-88B5F83D0513}']
+  end;
+
+  iControllerListBoxFactory = interface
+    ['{3C89A4E9-DF30-425B-8271-749F357D09CF}']
+  end;
 
 implementation
 
--- Menus.dpr
+++ Menus.dpr
@@ -5,7 +5,8 @@ uses
   FMX.Forms,
   Menus.Views.Principal in 'Views\Menus.Views.Principal.pas' {FormPrincipal},
   Menus.Controller.Interfaces in 'Controller\Menus.Controller.Interfaces.pas',
-  Menus.Model.Interfaces in 'Model\Menus.Model.Interfaces.pas';
+  Menus.Model.Interfaces in 'Model\Menus.Model.Interfaces.pas',
+  Menus.Controller.ListBox.Itens.Factory in 'Controller\Menus.Controller.ListBox.Itens.Factory.pas';
 
 {$R *.res}
 
